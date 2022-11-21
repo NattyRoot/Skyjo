61415336f8176441f40e3b1f2f6e86097dd1ad2e
Felix SOIGNON <felix.soignon@gmail.com>
2022-11-21 11:10:09 +0100

    Added unit tests for discard pile and creation of the board

--- src/test/java/fr/acensi/test/skyjo/SkyjoTest.java
+++ src/test/java/fr/acensi/test/skyjo/SkyjoTest.java
@@ -4,9 +4,10 @@ import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.ComponentUtil;
 import com.vaadin.flow.component.button.Button;
 import fr.acensi.skyjo.business.SkyjoLogic;
+import fr.acensi.skyjo.model.SkyjoBoard;
+import fr.acensi.skyjo.model.SkyjoCard;
 import fr.acensi.views.SkyjoView;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -28,8 +29,11 @@ public class SkyjoTest {
         SkyjoLogic.initDiscardPile();
     }
 
+    /**
+     * Test l'échange de cartes lors d'un Shift + Click
+     */
     @Test
-    public void whenSwapping_thenFieldHasChanged() {
+    public void whenSwapping_thenCardHasChangedPlace() {
         int expected = SkyjoLogic.getBoard().getPlayersField().get(0).getField()[0][0].getValue();
 
         Button button1 = SkyjoLogic.getBoard().getPlayersField().get(0).getField()[0][0].getButton();
@@ -44,8 +48,11 @@ public class SkyjoTest {
         Assert.assertEquals(expected, SkyjoLogic.getBoard().getPlayersField().get(0).getField()[0][1].getValue());
     }
 
+    /**
+     * Test la suppression d'une colonne
+     */
     @Test
-    public void whenRemoveColumn_thenFieldHasChanged() {
+    public void whenRemoveColumn_thenFieldHasOneLessColumn() {
         int expected = SkyjoLogic.getBoard().getPlayersField().get(0).getField().length;
 
         // Suppression de la colonne 0
@@ -54,6 +61,9 @@ public class SkyjoTest {
         Assert.assertEquals(expected - 1, SkyjoLogic.getBoard().getPlayersField().get(0).getField().length);
     }
 
+    /**
+     * Test le clique sur une carte
+     */
     @Test
     public void whenClickingCard_thenCardIsVisible() {
         SkyjoLogic.getBoard().getPlayersField().get(0).getField()[0][0].getButton().click();
@@ -61,15 +71,76 @@ public class SkyjoTest {
         Assert.assertTrue(SkyjoLogic.getBoard().getPlayersField().get(0).getField()[0][0].isVisible());
     }
 
+    /**
+     * Test le calcul de score
+     */
     @Test
     public void whenCalculatingScore_thenScoreIsCorrect() {
         // Au début de la partie, aucune carte n'est retourné donc le score est de 0
         Assert.assertEquals(0, SkyjoLogic.getBoard().getPlayersField().get(0).calculateScore());
 
-        int cardValue = SkyjoLogic.getBoard().getPlayersField().get(0).getField()[0][0].getValue();
+        int expected = SkyjoLogic.getBoard().getPlayersField().get(0).getField()[0][0].getValue();
         SkyjoLogic.getBoard().getPlayersField().get(0).getField()[0][0].getButton().click();
 
         // Après avoir retourné une carte, le score est forcément égale à la valeur de la carte
-        Assert.assertEquals(cardValue, SkyjoLogic.getBoard().getPlayersField().get(0).calculateScore());
+        Assert.assertEquals(expected, SkyjoLogic.getBoard().getPlayersField().get(0).calculateScore());
+    }
+
+    /**
+     * Test l'ajout dans la défausse
+     */
+    @Test
+    public void whenDiscardingCard_thenCardIsAddedInDiscardPile() {
+        // Au début de la partie, une seule carte est présente dans la défausse
+        Assert.assertEquals(1, SkyjoLogic.getBoard().getDiscardPile().getCards().size());
+
+        // On pioche une carte et on l'ajoute à la défausse
+        SkyjoCard card = SkyjoLogic.getBoard().getDeck().draw();
+        SkyjoLogic.getBoard().getDiscardPile().discard(card);
+
+        // Il y a maintenant deux cartes dans la défausse
+        Assert.assertEquals(2, SkyjoLogic.getBoard().getDiscardPile().getCards().size());
+    }
+
+    /**
+     * Test la recyclage de la défausse en pioche
+     */
+    @Test
+    public void whenRecyclingDiscardPile_thenDiscardPileIsEmptied() {
+        // On ajoute une carte à la défausse pour tester la fonctionnalité de recyclage de la défausse
+        SkyjoCard card = SkyjoLogic.getBoard().getDeck().draw();
+        SkyjoLogic.getBoard().getDiscardPile().discard(card);
+
+        // Il y a maintenant deux cartes dans la défausse
+        Assert.assertEquals(2, SkyjoLogic.getBoard().getDiscardPile().getCards().size());
+
+        int expected = card.getValue();
+
+        // On retoure la défausse mais on laisse la dernière carte ajoutée
+        SkyjoLogic.getBoard().turnDiscardPileIntoDeck();
+
+        // Il n'y a plus qu'une seule carte dans la défausse
+        Assert.assertEquals(1, SkyjoLogic.getBoard().getDiscardPile().getCards().size());
+        // La seule carte restante est la dernière ajoutée
+        Assert.assertEquals(expected, SkyjoLogic.getBoard().getDiscardPile().getTopCard().getValue());
+    }
+
+    /**
+     * Test la création du board et des fields
+     */
+    @Test
+    public void whenCreatingBoard_thenFieldIsCreatedForEachPlayers() {
+        // On créé un board pour deux personnes
+        SkyjoLogic.createBoard(2);
+        // On a donc deux playerFields
+        Assert.assertEquals(2, SkyjoLogic.getBoard().getPlayersField().size());
+
+        // On créé un board pour 10 personnes
+        SkyjoLogic.createBoard(10);
+        // On a donc 10 playerFields
+        Assert.assertEquals(10, SkyjoLogic.getBoard().getPlayersField().size());
+
+        // On ne peut pas créer de board pour 99 personnes car il n'y a que 150 cartes disponibles
+        Assert.assertThrows(IndexOutOfBoundsException.class, () -> SkyjoLogic.createBoard(99));
     }
 }
